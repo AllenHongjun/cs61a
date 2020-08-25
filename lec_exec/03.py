8173d986e5aaff05ebfb804ccf29440c39899d68
AllenHongjun <652971723@163.com>
2020-08-25 21:10:30 +0800

    04.py

--- lec_exec/03.py
+++ lec_exec/03.py
@@ -50,4 +50,119 @@ while i < 10:
 	#print('i: ',i,'total: ',total)
 total
 
+def end(n, d):
+	"""Print the final digits of N in revierse order until D is found.
+	倒叙打印n的数字 直到d这个数字被找到
 
+	>>> end(234324,3)
+	4
+	2
+	3
+	"""
+	while n > 0:
+		last, n = n % 10, n // 10
+		### 先打印数字 ，如果答应的数字是最后一个数字就结束返回
+		print(last)
+		if d == last:
+			return None
+
+
+
+
+def is_three(x):
+	"""Return whether x is three
+
+	>>> search(is_three)
+	3
+	"""
+	return x == 3
+
+
+
+def positive(x):
+	"""A function that is 0 until square(x) - 100 is positive
+
+	>>> search(positive)
+	11
+	"""
+
+	return max(0,square(x) - 100)
+
+
+# 函数作为一个参数返回
+def search(f):
+	"""Return the smallest non-negative interger x for which f(x) is a true value."""
+	x = 0
+	while True:
+		if f(x):
+			return x
+		x += 1
+
+
+def square(x):
+	return x * x
+
+### doctest详细内容如何来显示
+### lambda表达式如何理解
+### 这个代码如何单不吊事如何运行
+def invert(f):
+	"""Return a function g(y) that returns x such that f(x) == y.
+
+	>>> sqrt = invert(square)
+	>>> sqrt(16)
+	4
+	"""
+	return lambda y : search(lambda x : f(x) == y)
+
+
+# Control
+def if_(c, t, f):
+	if c:
+		t
+	else:
+		f
+
+from math import sqrt
+
+def real_sqrt(x):
+	"""Return the real part of the square root of x.
+
+	>>> real_sqrt(4)
+	2.0
+	>>> real_sqrt(-4)
+	0.0
+	"""
+	if x > 0:
+		return sqrt(x)
+	else:
+		return 0.0
+
+def has_big_sqrt(x):
+	"""Return whether x has a big square root.
+
+	>>> has_big_sqrt(1000)
+	True
+	>>> has_big_sqrt(100)
+	False
+	>>> has_big_sqrt(0)
+	False
+	>>> has_big_sqrt(-1000)
+	False
+	"""
+	return x > 0 and sqrt(x) > 10
+
+
+def reasonable(n):
+	"""Is N small enough that 1/N can be represented?
+
+	>>> reasonable(100)
+	True
+	>>> reasonable(0)
+	True
+	>>> reasonable(-100)
+	True
+	>>> reasonable(10 ** 100)
+	False
+	"""
+
+	return n == 0 or 1 / n != 0.0
